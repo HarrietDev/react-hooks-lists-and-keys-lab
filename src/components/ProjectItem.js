69814b69e8d00692cc6970e72cb5121a72838910
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-05-04 01:40:29 +0300

    working with array and keys

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -1,4 +1,6 @@
 import React from "react";
+import ProjectList from "./ProjectList";
+import user from "../data/user";
 
 function ProjectItem({ name, about, technologies }) {
   return (
@@ -6,7 +8,9 @@ function ProjectItem({ name, about, technologies }) {
       <h3>{name}</h3>
       <p>{about}</p>
       <div className="technologies">
-        {/* render a <span> for each technology in the technologies array */}
+        {technologies.map((technology) =>(
+        <span key={technology}>{technology}</span>
+      ))}
       </div>
     </div>
   );
